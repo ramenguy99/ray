443368a9b633f6c0c76c9f91be41616b2c672479
Dario Mylonopoulos <32958057+ramenguy99@users.noreply.github.com>
2021-02-24 23:22:08 +0100

    Update README.md

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Ray is a simple ray tracer written from scratch in C++ to learn about graphics programming.
 It's written using a small subset of C++ very similar to C.
 
+![Dragon and spheres](https://github.com/ramenguy99/ray/blob/master/images/dragons.png)
+
 ## Features
 - Ray intersection with spheres, planes and triangle meshes
 - Bounding volume hierarchies to speed up ray-mesh intersection
@@ -35,12 +37,11 @@ cd build
 ray out.bmp
 ```
 
+Use `ray -h` for information about parameters
+
 # Acknowledgements
 
 The dragon model in `res/dragon.dae` is a reformat of the scan from Stanford University Computer Graphics Laboratory. Redistribution of the model is allowed for non commercial purposes. The original model and additional information is available at http://graphics.stanford.edu/data/3Dscanrep/
 
 Some of the intersection tests and geometry procedures are from Real-Time Collision Detection, CRC Press 2004 by Christer Ericson
 
-# Example
-
-![Dragon and spheres](https://github.com/ramenguy99/ray/tree/master/images/dragon.png)
--- build/build.bat
+++ build/build.bat
@@ -3,34 +3,34 @@
 REM Hopefully find visual studio vcvarsall file to add the compiler to the path
 
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 12.0
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" amd64))
 
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 11.0
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" amd64))
 
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" amd64))
 
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 13.0
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" amd64))
 
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" amd64))
 
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" amd64))
 
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" amd64))
 
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" amd64))
 
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" amd64))
 
 IF NOT DEFINED LIB ( 
-    echo Unable to find Visual Studio installation. Execute   vcvarsall.bat amd64   in the shell before building
+    echo Unable to find Visual Studio installation. Execute vcvarsall.bat amd64 in the shell before building
     EXIT /B 1
 )
 
